503ffe4a21f0db6145d3adaba482c60ea849d8ba
opeyemibami <bamigbadeopeyemi@gmail.com>
2024-03-14 08:44:32 +0000

    deployment prep

--- config/getWbInstance.py
+++ config/getWbInstance.py
@@ -1,9 +1,20 @@
 from selenium import webdriver as wd
+from selenium.webdriver.common.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 def getWB():
+    chrome_options = wd.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # wb = wd.Chrome() 
     wb = wd.Chrome() 
+
     wb.implicitly_wait(10)
     wb.get(config["URL"])
     return wb
